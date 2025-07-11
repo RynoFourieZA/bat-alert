c46997ebe587d0694192adf106629d7d538383e4
Ryno Fourie <rjrfourie@outlook.com>
2025-07-11 16:52:01 +0200

    feat: add session storage

--- src/auth/ProtectedRoutes.tsx
+++ src/auth/ProtectedRoutes.tsx
@@ -1,9 +1,33 @@
 import { Outlet, Navigate } from "react-router";
-import { useAppSelector } from "../app/hooks";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { setStatus, setToken, type StatusType, type TokenType } from "../features/authSlice";
 
 const ProtectedRoutes = () => {
-    const status = useAppSelector(state => state.auth.status);
-    return status === "success" ? <Outlet /> : <Navigate to="/login" />
-}
+  const dispatch = useAppDispatch();
+
+  let status = useAppSelector((state) => state.auth.status);
+  let token = useAppSelector((state) => state.auth.token);
+
+  const storedStatus = sessionStorage.getItem("status");
+  const storedToken = sessionStorage.getItem("token");
+
+  let getSessionStatus: StatusType;
+  let getSessionToken: TokenType;
+
+  if (storedStatus !== null && storedToken !== null) {
+    getSessionStatus = { status: storedStatus };
+    getSessionToken = { token: storedToken };
+    dispatch(setStatus(getSessionStatus));
+    dispatch(setToken(getSessionToken));
+  } else {
+    getSessionStatus = { status: "" }; 
+  }
+
+  return status === "success" || storedStatus === "success" && token !== "" ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
 
 export default ProtectedRoutes;
